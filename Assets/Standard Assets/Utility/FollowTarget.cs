1e7f0696493f8b014a51f0b5c3ff82a87ba4ee00
Axel Vencatareddy <avencatareddy@me.com>
2017-01-21 18:12:43 +0100

    Update speed of sphere movements

--- Assets/Standard Assets/Utility/FollowTarget.cs
+++ Assets/Standard Assets/Utility/FollowTarget.cs
@@ -10,24 +10,29 @@ namespace UnityStandardAssets.Utility
         public Transform target;
         public Vector3 offset = new Vector3(0f, 0f, 0f);
 		public float max = 0f;
+		public float speed = 0.4f;
 
         private void LateUpdate()
         {
-			if (Input.GetKey ("space") && target.position.y < 20f) {
-				offset.y = 1f;
+			if (Input.GetKey ("space") && target.position.y < 19f) {
+				offset.y = speed;
 				max = 0f;
-			} else if (target.position.y > -max && max != 0f) {
+			} else if (target.position.y > -(max) && max != 0f) {
 				print ("max" +  max);
-				offset.y = -1f;
+				offset.y = -speed;
 			} else if (target.position.y < 0f) {
-				offset.y = 1f;
+				if (target.position.y + speed > 0f) {
+					offset.y = -target.position.y;
+				} else {
+					offset.y = speed;
+				}
 			} else {
 				offset.y = 0f;
 			}  
             transform.position = target.position + offset;
 			if (target.position.y > 0f && target.position.y > max) {
 				max = target.position.y;
-			} else if (max > 0f && target.position.y == -max) {
+			} else if (max > 0f && target.position.y <= -(max)) {
 				max = 0f;
 			}
         }
