3d21f93b6bca9082f35dde747c6ed717293ae446
lvezia <louis.vezia@epitech.eu>
2017-01-22 17:29:40 +0100

    Added spawning blocs

---
+++ Assets/Standard Assets/blocManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blocManager : MonoBehaviour {
+
+    public Transform    bloc;
+    public float        spawnDelay = 3f;
+    public float        spawnTime = 2f;
+    float timer = 0f;
+    
+	void Start () {
+	}
+	
+	void Update () {
+        if (spawnDelay <= Time.realtimeSinceStartup)
+        {
+            if (timer == 0f)
+            {
+                timer = Time.realtimeSinceStartup;
+            }
+            if (spawnTime >= 0.5f && timer + spawnTime <= Time.realtimeSinceStartup)
+            {
+                timer = Time.realtimeSinceStartup;
+                Spawn();
+                if (0.5f <= 2f - (Time.realtimeSinceStartup / 20))
+                    spawnTime = 2f - (Time.realtimeSinceStartup / 20);
+                else
+                    spawnTime = 0.5f;
+            }
+        }
+    }
+
+    void Spawn() {
+        Transform newGameObject = Instantiate(bloc);
+    }
+}
--- Assets/Standard Assets/moveScript.cs
+++ Assets/Standard Assets/moveScript.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class moveScript : MonoBehaviour {
     
-    public Vector2 speed = new Vector2(50, 50);
-    private Vector2 direction = new Vector2(-1, 0);
+    public Vector2 speed = new Vector2(10, 10);
+    private Vector2 direction = new Vector2(-1f, 0);
     private Vector2 movement;
+
+    void Start()
+    {
+        gameObject.transform.position = new Vector3(40, Random.Range(-19, 19), 0);
+    }
 
     void Update()
     {
         movement = new Vector2(
           speed.x * direction.x,
-          speed.y * direction.y);
+          0);
 
     }
 
     void FixedUpdate()
     {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(gameObject.transform.position);
         GetComponent<Rigidbody>().velocity = movement;
+        if (screenPos.x <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
